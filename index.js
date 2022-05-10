5bec54adaf2e577b6d6522466935ca8b8ef0b6b9
Bakare Faruq <bakarefaruq.a@gmail.com>
2022-05-10 19:26:57 +0100

    fixed

--- index.js
+++ index.js
@@ -1,20 +1,96 @@
- 
-let passwordbox = document.getElementById("passwordbox")
-function genPassword(){
-    let chars=["0", "1","2", "3", "4", "5", "6", "7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
-    "t","u","v","w","x","y","z","!","@","#","$","%","^","&","*","(",")","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    let passwordLength= document.getElementById("length");
-    let password="";
-   for (let i = 0; i <= passwordLength; i++) {
-    let generate = chars[Math.floor(Math.random() * chars.length)];
-      password += generate
-    }
-    return pasword
+const chars = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const generatedPasswordContainers =
+  document.querySelectorAll(".generatedPassword");
+const passwordLengthInput = document.querySelector("#length");
 
+function genPassword() {
+  // console.log("password length: ", passwordLengthInput);
+  // console.log("password length: ", generatedPasswordContainers);
+  let passwordLength = Number(passwordLengthInput.value);
+  for (let i = 0; i < generatedPasswordContainers.length; i++) {
+    generatedPasswordContainers[i].value = getRandomPassword(passwordLength);
+  }
 }
 
-
-
-
-
-
+function getRandomPassword(length) {
+  let password = "";
+  for (let i = 0; i <= length; i++) {
+    let generate = chars[Math.floor(Math.random() * chars.length)];
+    password += generate;
+  }
+  console.log("generated password: ", password);
+  return password;
+}
